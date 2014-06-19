91d8fc1d9401eed34a51f7d08cca4074db6ad2fd
Viljami Salminen <viljami.salminen@gmail.com>
2014-06-19 12:53:58 -0400

    Initial version

---
+++ moltenleading.js
@@ -0,0 +1,209 @@
+(function (window, document, undefined) {
+
+	"use strict";
+
+	/**
+	 * The MoltenLeading object
+	 *
+	 * @param	{String} CSS selector for target elements
+	 * @param	{Object} MoltenLeading options
+	 * @constructor
+	 */
+	function MoltenLeading(selector, options) {
+		if (!selector) {
+			throw new Error("No selector supplied for Molten Leading");
+		}
+
+		this.selector = selector;
+		this.options = this.extend({}, MoltenLeading.defaultOptions, options);
+
+		// If querySelector is supported, use that
+		if ("querySelector" in document && document.querySelector(this.selector)) {
+			this.elements = document.querySelectorAll(this.selector);
+		// If querySelector is not supported, use getElementsByTagName (for IE7 & 6)
+		} else if (document.getElementsByTagName(this.selector)) {
+			this.elements = document.getElementsByTagName(this.selector);
+    // If element doesn't exists at all, give error
+    } else {
+      throw new Error("The element you are trying to select for Molten Leading doesn't exist");
+    }
+	}
+
+	/**
+	 * Default options
+	 */
+	MoltenLeading.defaultOptions = {
+		minline: 1.2,   // Minimum line height
+		maxline: 1.8,	 // Maximum line height
+		minwidth: 320,  // Minimum element width where the adjustment starts
+		maxwidth: 1024  // Maximum element width where the adjustment stops
+	};
+
+	MoltenLeading.prototype = {
+	  constructor : MoltenLeading,
+
+		/**
+	   * Intializes the instance
+		 *
+		 * @function
+	   */
+		init : function () {
+			this.forEach(this.elements, this.hotlead, this);
+			this.addEvent(window, "resize", this, false);
+		},
+
+		/**
+		* Calculates the appropriate leading
+		*
+		* @param	{index} the index of the element
+		* @param	{Element} the element to calculate the leading for
+		* @return {string} the calculated leading for the element that was selected
+		*/
+		hotlead : function (i, el) {
+			var o = this.options;
+			var widthperc = parseInt((el.offsetWidth - o.minwidth) / (o.maxwidth - o.minwidth) * 100, 10);
+			var linecalc = o.minline + (o.maxline - o.minline) * widthperc / 100;
+
+			if (widthperc <= 0 || linecalc < o.minline) {
+				linecalc = o.minline;
+			} else if (widthperc >= 100 || linecalc > o.maxline) {
+				linecalc = o.maxline;
+			}
+
+			el.style.lineHeight = linecalc;
+		},
+
+		/**
+	   * Handles window resizing and calls `hotlead` when appropriate
+	   *
+	   * @function
+	   */
+		resize : function () {
+			this.debounce(function () {
+				this.forEach(this.elements, this.hotlead, this);
+			}, 100);
+		},
+
+		/**
+		* Merge object into target
+		*
+		* @param {target} the object for other objects to be merged into
+		* @returns {Object} the merged object (a reference to target)
+		*/
+		extend : function (target) {
+		  var args = arguments;
+
+		  for (var i = 1; i < args.length; i++) {
+		    var source = args[i];
+		    for (var property in source) {
+		      if (source.hasOwnProperty(property)) {
+		        target[property] = source[property];
+		      }
+		    }
+		  }
+
+		  return target;
+		},
+
+		/**
+		* Debounce returns a function, that, as long as it continues to be invoked,
+		* will not be triggered. The function will be called after it stops being
+		* called for N milliseconds. If `immediate` is passed, trigger the function
+		* on the leading edge, instead of the trailing.
+		*/
+		debounce : function (func, wait) {
+			var timeout;
+			var context = this, args = arguments;
+			var later = function () {
+				timeout = null;
+			};
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			func.apply(context, args);
+		},
+
+		/*
+		* Handles events in a way that they work in all browsers
+		*
+		* fn arg can be an object or a function, thanks to handleEvent
+		* read more at: http://www.thecssninja.com/javascript/handleevent
+		*
+		* @param	{Element} the element to add the eventListener for
+		* @param	{string}  the eventListener which should be added
+		* @param	{string}	the context that should be passed
+		* @param	{boolean} event bubbling, true or false
+		*/
+		addEvent : function (el, evt, fn, bubble) {
+			if ("addEventListener" in el) {
+				// BBOS6 doesn't support handleEvent, catch and polyfill
+				try {
+					el.addEventListener(evt, fn, bubble);
+				} catch (e) {
+					if (typeof fn === "object" && fn.handleEvent) {
+						el.addEventListener(evt, function (e) {
+							// Bind fn as this and set first arg as event object
+							fn.handleEvent.call(fn, e);
+						}, bubble);
+					} else {
+						throw e;
+					}
+				}
+			} else if ("attachEvent" in el) {
+				// check if the callback is an object and contains handleEvent
+				if (typeof fn === "object" && fn.handleEvent) {
+					el.attachEvent("on" + evt, function () {
+						// Bind fn as this
+						fn.handleEvent.call(fn);
+					});
+				} else {
+					el.attachEvent("on" + evt, fn);
+				}
+			}
+		},
+
+		/**
+		 * forEach method
+		 *
+		 * @param  {array} array to loop through
+		 * @param  {function} callback function
+		 * @param  {object} scope
+		 */
+		forEach : function (array, callback, scope) {
+			for (var i = 0; i < array.length; i++) {
+				callback.call(scope, i, array[i]);
+			}
+		},
+
+		/**
+		* Handling the events
+		*
+		* @param  {event} event
+		* @return {function} Returns appropriate function to be used with the event
+		*/
+		handleEvent : function (event) {
+			var evt = event || window.event;
+
+			switch (evt.type) {
+			case "resize":
+				this.resize(evt);
+				break;
+			}
+		}
+
+	};
+
+	/**
+	 * Expose a public-facing API
+	 *
+	 * @param  {String} CSS selector for target elements
+	 * @param  {Object} the options
+	 */
+	function expose(selector, options) {
+	  var mtl = new MoltenLeading(selector, options);
+	  MoltenLeading.currentOptions = options;
+	  mtl.init();
+	}
+
+	window.moltenLeading = expose;
+
+}(window, document));
--- readme.md
+++ readme.md
@@ -1,7 +1,40 @@
-# Slingin’ Hot Leading
-Manually adjusting ```line-height``` for optimum readability across a bunch of media queries is kind of a pain. So I made a thing. One sets a minimum width at which the adjustment starts, a maximum element width where it stops, and a minimum and maximum line height to adjust through. This isn’t especially well-tested, but seems to check out in all the big desktop browsers (including IE).
+# Molten Leading (Vanilla JavaScript version)
+
+Manually adjusting ```line-height``` for optimum readability across a bunch of media queries is kind of a pain. With Molten Leading you can set a minimum width at which the adjustment starts, a maximum element width where it stops, and a minimum and maximum line height to adjust through.
+
+All the work here is based on [@Wilto’s Molten-Leading](https://github.com/Wilto/Molten-Leading) jQuery plugin.
+
+
+# Usage instructions
+
+Following the steps below you should be able to get everything up and running.
+
+1. Link files:
+```html
+	<script src="moltenleading.js"></script>
+```
+
+2. Hook up the plugin:
+```html
+<!-- Put this right before the </body> closing tag -->
+	<script>
+		moltenLeading("h1");
+	</script>
+```
+
+4. Customizable options:
+```javascript
+	moltenLeading("h1", {
+   minline: 1.2,  // Minimum line height
+   maxline: 1.8,  // Maximum line height
+   minwidth: 320, // Minimum element width where the adjustment starts
+   maxwidth: 1024 // Maximum element width where the adjustment stops
+ });
+```
+
 
 ## Notes:
-* Binding to ```resize``` makes me nauseous to begin with, and in this first pass I’m going about it in a particularly gross way.
+* Tested to be working all the way down to IE6. side note: if you need to support IE6 & 7 you’re gonna have to use "tag selectors", since the plugin uses getElementsByTagName as a fallback if querySelector isn’t supported.
+* Built Progressive Enhancement in mind, so the plugin will silently fail when a browser doesn’t support certain selector (looking at IE6 & 7).
 * There’s a demo <a href="http://wilto.github.com/Molten-Leading/">right hurr</a>.
-* Full credit to <a href="http://twitter.com/nicewebtype">Tim Brown</a> for <a href="http://nicewebtype.com/notes/2012/02/03/molten-leading-or-fluid-line-height/">the post</a> that got my lazy ass in gear on this, and the awesome name.
+* Full credits go to both <a href="http://twitter.com/nicewebtype">Wilto</a> who wrote the orinal plugin and to <a href="http://twitter.com/nicewebtype">Tim Brown</a> for <a href="http://nicewebtype.com/notes/2012/02/03/molten-leading-or-fluid-line-height/">the original idea</a>.
