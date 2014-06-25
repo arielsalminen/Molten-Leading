790b90715c3506e717df7118d02e38573bf95988
Viljami Salminen <viljami.salminen@gmail.com>
2014-06-25 22:15:01 +0300

    Use requestAnimationFrame for better performance

--- moltenleading.js
+++ moltenleading.js
@@ -1,5 +1,5 @@
 /*!
- * Molten Leading, plain JavaScript version, v1.03
+ * Molten Leading, plain JavaScript version, v1.1
  * https://github.com/viljamis/Molten-Leading
  */
 (function (window, document, undefined) {
@@ -9,11 +9,12 @@
   /**
    * The MoltenLeading object
    *
-   * @param	{String} CSS selector for target elements
-   * @param	{Object} MoltenLeading options
+   * @param  {String} CSS selector for target elements
+   * @param  {Object} MoltenLeading options
    * @constructor
    */
   function MoltenLeading(selector, options) {
+    this.ticking = false;
     this.selector = selector;
     this.options = this._extend({}, MoltenLeading.defaultOptions, options);
 
@@ -31,12 +32,37 @@
    * Default options
    */
   MoltenLeading.defaultOptions = {
-    minline: 1.2,    // Minimum line-height for the element, numbers (multiplied by the element's font-size)
-    maxline: 1.8,    // Maximum line-height for the element, numbers (multiplied by the element's font-size)
+    minline: 1.2,    // Minimum line-height for the element, number (multiplied by the element's font-size)
+    maxline: 1.8,    // Maximum line-height for the element, number (multiplied by the element's font-size)
     minwidth: 320,   // Minimum element width where the adjustment starts, pixels
-    maxwidth: 1024,  // Maximum element width where the adjustment stops, pixels
-    threshold: 100   // Threshold time used on window resize, milliseconds
+    maxwidth: 1024   // Maximum element width where the adjustment stops, pixels
   };
+
+  /**
+   * Shim layer for requestAnimationFrame with setTimeout fallback
+   */
+  window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+      function (callback) { window.setTimeout(callback, 1000 / 60); };
+  })();
+
+  /**
+   * Polyfill bind() for old browsers
+   */
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+      var aArgs = Array.prototype.slice.call(arguments, 1);
+      var fToBind = this;
+      var fNOP = function () {};
+      var fBound = function () {
+        return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+               aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+      return fBound;
+    };
+  }
 
   MoltenLeading.prototype = {
     constructor : MoltenLeading,
@@ -47,23 +73,38 @@
      * @function
      */
     init : function () {
-      this.resize();
-      this._addEventListener("resize", this._debounce(onResize, this.options.threshold));
-
-      var self = this;
-      function onResize(event) {
-        self.resize(event);
-      }
+      this.update();
+      this._addEventListener(window, "resize", this, false);
     },
 
     /**
-    * Calculates the appropriate leading
+     * Updates leading when needed
+     *
+     * @function
+     */
+    update : function () {
+      this._forEach(this.elements, this._calcLeading, this);
+      this.ticking = false;
+    },
+
+    /**
+     * Attach this as the event listener
+     *
+     * @function
+     */
+    handleEvent : function () {
+      this._requestTick();
+    },
+
+    /**
+    * Calculates the appropriate leading and updates DOM
     *
-    * @param	{index} the index of the element
-    * @param	{Element} the element to calculate the leading for
+    * @param  {index} the index of the element
+    * @param  {Element} the element to calculate the leading for
     * @return {string} the calculated leading for the element
+    * @private
     */
-    hotlead : function (i, el) {
+    _calcLeading : function (i, el) {
       var elwidth = el.offsetWidth;
       var widthperc = parseInt((elwidth - this.options.minwidth) / (this.options.maxwidth - this.options.minwidth) * 100, 10);
       var linecalc = this.options.minline + (this.options.maxline - this.options.minline) * widthperc / 100;
@@ -78,15 +119,6 @@
     },
 
     /**
-     * Handles window resizing and calls `hotlead` when appropriate
-     *
-     * @function
-     */
-    resize : function () {
-      this._forEach(this.elements, this.hotlead, this);
-    },
-
-    /**
     * Merge object into target
     *
     * Using copy of the array and item in for loop is FAST:
@@ -94,13 +126,14 @@
     *
     * @param {target} the object for other objects to be merged into
     * @returns {Object} the merged object (a reference to target)
+    * @private
     */
     _extend : function (target) {
       var args = arguments;
       var copyOfArray = [];
 
-      for (var i = 0, item; item = args[i]; i++) {
-        var source = copyOfArray[i] = item;
+      for (var i = 1; i < args.length; i++) {
+        var source = args[i];
         for (var property in source) {
           if (source.hasOwnProperty(property)) {
             target[property] = source[property];
@@ -117,45 +150,44 @@
      * @param  {array} array to loop through
      * @param  {function} callback function
      * @param  {object} scope
+     * @private
      */
     _forEach : function (array, callback, scope) {
-      var copyOfArray = [];
-      for (var i = 0, item; item = array[i]; i++) {
-        callback.call(scope, i, copyOfArray[i] = item);
+      for (var i = 0; i < array.length; i++) {
+        callback.call(scope, i, array[i]);
       }
     },
 
     /**
-     * Debounce returns a function, that, as long as it continues to be invoked,
-     * will not be triggered. The function will be called after it stops being
-     * called for N milliseconds.
+     * Calls rAF if it hasn't been already called,
+     * ensuring events don't get stacked
      *
      * @param  {function} callback function
-     * @param  {integer} milliseconds to wait
+     * @private
      */
-    _debounce : function (func, wait) {
-      var timeout;
-      return function () {
-        var context = this, args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          timeout = null;
-          func.apply(context, args);
-        }, wait);
-      };
+    _requestTick : function (callback) {
+      if (!this.ticking) {
+        requestAnimFrame(this.update.bind(this));
+        this.ticking = true;
+      }
     },
 
     /**
      * Adds event listeners
      *
+     * @param  {element} the element which the even should be added to
      * @param  {event} the event to be added
      * @param  {function} the function that will be executed
+     * @private
      */
-    _addEventListener : function (event, listener) {
-      if ("addEventListener" in window) {
-        window.addEventListener("resize", listener, false);
-      } else if ("attachEvent" in window) {
-        window.attachEvent("on" + event, listener);
+    _addEventListener : function (el, evt, fn, bubble) {
+      if ("addEventListener" in el) {
+        el.addEventListener(evt, fn, bubble);
+      } else if ("attachEvent" in el) {
+        el.attachEvent("on" + evt, function () {
+          // Bind fn as this
+          fn.handleEvent.call(fn);
+        });
       }
     }
 
